2c0ee9cd8f93a0b03402ea0e6a01b1c7a1a25450
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-07 12:47:46 -0700

    [add] inventory_index

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from datetime import datetime
+
+app = Flask(__name__)
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/FanGear')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+inventory = db.inventory
+
+
+
+@app.route('/')
+def inventory_index():
+    """Show all playlists."""
+    return render_template('inventory_index.html', inventory=inventory.find())

8414b3af30e1344c01ed63846666ff0a4b5bb390
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-08 20:00:46 -0700

    [add] inventory

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from inventory import Store
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,11 +10,21 @@ app = Flask(__name__)
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/FanGear')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-inventory = db.inventory
+inventory = db['inventory']
 
-
+store = Store(inventory)
+store.show_inventory()
 
 @app.route('/')
 def inventory_index():
     """Show all playlists."""
-    return render_template('base.html', inventory=inventory.find())
+    return render_template('show_inventory.html', inventory_list=inventory.find())
+
+
+@app.route('/inventory/')
+def show_inventory():
+    items = inventory.find({'product_name': 'Official NBA Jersey'})
+    return render_template("show_inventory.html", inventory=items)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
---
+++ inventory.py
@@ -0,0 +1,52 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+product_1 = {
+    'product_team_brand': 'Golden State Warriors',
+    'product_name' : 'Official NBA Jersey',
+    'pic_path' : './static/warriors_jersey.jpeg'
+}
+
+product_2 = {
+    'product_team_brand': 'Los Angeles Lakers',
+    'product_name' : 'Official NBA Jersey',
+    'pic_path' : './static/lakers_jersey.jpeg'
+}
+
+product_3 = {
+    'product_team_brand': 'L.A. Clippers',
+    'product_name' : 'Official NBA Jersey',
+    'pic_path' : './static/clippers_jersey.jpeg'
+}
+
+product_4 = {
+    'product_team_brand': 'Portland Trail Blazers',
+    'product_name' : 'Official NBA Jersey',
+    'pic_path' : './static/trail_blazers_jersey.jpeg'
+}
+
+product_5 = {
+    'product_team_brand': 'Brooklyn Nets',
+    'product_name' : 'Official NBA Jersey',
+    'pic_path' : './static/nets_jersey.jpeg'
+}
+
+inventory_list = [
+    product_1,
+    product_2,
+    product_3,
+    product_4,
+    product_5
+]
+
+class Store():
+    def __init__(self, inventory):
+        self.inventory = inventory
+    
+    def show_inventory(self):
+        self.inventory.delete_many({})
+        
+        self.inventory.insert_many(inventory_list)
+        
+        for i in self.inventory.find():
+            print(inventory_list)

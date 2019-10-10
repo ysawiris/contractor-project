57b80f8cd6deed60731a5085c8d509cc22fe1010
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-09 17:56:07 -0700

    [add] review feature

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/FanGear')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-inventory = db['inventory']
+inventory = db.inventory
+comments = db.comments 
 
 store = Store(inventory)
 store.show_inventory()
@@ -20,7 +21,6 @@ def inventory_index():
     """Show all playlists."""
     return render_template('show_inventory.html', inventory_list=inventory.find())
 
-
 @app.route('/inventory/')
 def show_inventory():
     items = inventory.find({'product_name': 'Official NBA Jersey'})
@@ -29,7 +29,27 @@ def show_inventory():
 @app.route('/inventory/<inventory_id>')
 def show_item(inventory_id):
     item = inventory.find_one({'_id': ObjectId(inventory_id)})
-    return render_template("item_show.html", inventory = item)
+    item_comments = comments.find({'inventory_id': ObjectId(inventory_id)})
+    return render_template("item_show.html", inventory = item, comments = item_comments)
+
+@app.route('/inventory/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'inventory_id': ObjectId(request.form.get('inventory_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('show_item', inventory_id=request.form.get('inventory_id')))
+
+@app.route('/inventory/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('show_item', inventory_id=comment.get('inventory_id')))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))

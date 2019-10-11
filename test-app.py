5808997c9c3c36a8001b7c7cfc79b314e44ecc39
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-10 18:26:07 -0700

    add test routes

---
+++ test-app.py
@@ -0,0 +1,25 @@
+# tests.py
+
+from unittest import TestCase, main as unittest_main
+from app import app
+
+class ContractorTests(TestCase):
+    """Flask tests."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+    
+    def test_index(self):
+        """Test the FanGear homepage."""
+        result = self.client.get('/')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Jersey', result.data)
+
+if __name__ == '__main__':
+    unittest_main()
